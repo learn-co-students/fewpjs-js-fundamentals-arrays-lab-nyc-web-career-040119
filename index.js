8ff8202b50b1adadc71f45f861ec12d80f544db4
Daviicode <d.ydzdavid@yahoo.com>
2019-05-13 17:03:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+var array = ["Milo", "Otis", "Garfield"];
+
+
+function append (array) {
+  return array.push("Odie");
+}
+
+function prepend (array) {
+  return array.unshift('pet');
+}
+
+function removeLast (array) {
+  return array.pop();
+}
+
+function removeFirst (array) {
+  return array.unshift();
+}
